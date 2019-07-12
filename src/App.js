ad5c6d37b5c3d7f7d8a889f505e3083dbc300d61
geraintwilliams <geraint.williams@gmail.com>
2019-07-12 13:00:11 +0100

    add attribute for override

--- src/App.js
+++ src/App.js
@@ -8,35 +8,42 @@ class GenericTextBox extends React.Component {
   }
 
   onInputChange(e){
-
     const value = e.target.value;
     
     /* call the parent's onChange */
     this.props.onChange(value);
 
-    /* handle onChange locally */
-    console.log('make http call with', value);
-
+    if (this.props.overrideOnChange) {
+    }
+    else {
+      /* handle onChange locally */
+      console.log('make http call with', value);
+    }
   }
 
   render() { return <div>hello<input type="text" onChange={this.onInputChange}/></div> }
+
 }
 
 class MyTextBox extends React.Component {
+
   constructor(props){
     super(props);
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(value){
+
     /* call parent */
     this.props.onChange(value);
+
+    /* handle onChange locally */
+
     console.log('make super special http call with', value, 'and DATE');
   }
 
-  render() { return <GenericTextBox
-    onChange={this.onInputChange}
-      /> }
+  render() { return <GenericTextBox onChange={this.onInputChange} overrideOnChange="true" /> }
+
 }
 
 class MyForm extends React.Component {
@@ -49,20 +56,20 @@ class MyForm extends React.Component {
 }
 
 class ExistingForm extends React.Component {
+
   handleChange(value){
     console.log('set state on form', value)
   }
 
   render() { return <form> <GenericTextBox onChange={this.handleChange} /> </form> }
+
 }
 
 function App() {
   return (
     <div className="App">
-
       <MyForm/>
       <ExistingForm/>
-
     </div>
   );
 }
